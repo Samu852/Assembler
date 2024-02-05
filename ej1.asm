f77bb642a4e33062034f40114aa20b583405cdfe
Samuel Arango <samuel.arangob@upb.edu.co>
2024-02-05 17:43:35 -0500

    Creo carpeta figures con imagen de los comandos principales + ejemplos

---
+++ ej1.asm
@@ -0,0 +1,20 @@
+D=1
+D=D+1
+@1954
+D=A
+@23
+D=D+A
+@17
+D+A
+@100
+M=D
+@200 // apunta al address 200
+D=M  // Copia el valor en la memoria 200 a D
+@100 // apunta al adress 100
+M=D // copia el valor en D a la memoria 100
+@3
+D=M
+@15
+D=D-A
+@3
+M=D
